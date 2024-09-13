98c5a1bc5cc8c68988c4bf12886531d51e89b193
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-09-13 11:19:49 +0300

    sidemenu scroll
    sidemenu required some goblin magic

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, Divider } from "antd";
+import { Button } from "antd";
 import { DoubleRightOutlined, DoubleLeftOutlined } from "@ant-design/icons";
 
 export default function SideMenu({ children }: { children: React.ReactNode }) {
@@ -12,17 +12,19 @@ export default function SideMenu({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <div className="relative z-50 ">
+    <div className="relative z-50 h-screen">
       <div
-        className={`h-screen xl:w-1/3 lg:w-2/5 md:w-3/4 sm:w-5/6 bg-[#f0f0f0]  fixed right-0 top-0 transition-transform duration-300 ease-in-out ${
+        className={`h-full fixed right-0 top-0 xl:w-1/3 lg:w-2/5 md:w-3/4 sm:w-5/6 bg-[#f0f0f0] transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}>
         <Button
-          className="absolute left-[-30px] top-1/2 transform -translate-y-1/2  bg-orange-600 text-white px-3 py-2 rounded-r-md"
+          className="z-50 absolute left-[-50px] top-1/2 transform -translate-y-1/2 bg-orange-600 text-white p-6 rounded-r-md"
           onClick={toggleSidebar}>
           {isOpen ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
         </Button>
-        <div className="mx-2 text-white">{children}</div>
+        <div className="overflow-y-scroll h-full">
+          <div className="mx-2 text-white">{children}</div>
+        </div>
       </div>
     </div>
   );
