6e7c9887746204140751b8281f4efef91f869f62
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-09-23 14:09:40 +0300

    update faq

--- src/app/female2/page.tsx
+++ src/app/female2/page.tsx
@@ -1,13 +1,10 @@
 import Contact from "@/components/spaghetti/Contact";
 import Doctors from "@/components/spaghetti/Doctors";
-import Hero from "@/components/spaghetti/Hero";
-import HeroDynamic from "@/components/spaghetti/HeroDynamic";
 import Location from "@/components/spaghetti/Location";
 import OperationForm from "@/components/spaghetti/OperationForm";
 import ParentState from "@/components/spaghetti/ParentState";
 import { Divider } from "antd";
 import { Metadata } from "next";
-import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "LP Title",
@@ -21,13 +18,34 @@ const buttonsData = [
     image2: "/BeforeAfter/breast2.webp",
     faqs: [
       {
-        question: "What is Hair Transplant?",
+        question: "How do I know which breast surgery is right for me?",
         answer:
-          "Hair Transplant is a procedure to restore hair growth. Hair Transplant is a procedure to restore hair growth.",
+          "Our surgeons, led by Dr. Bulent Cihantimur, offer personalized consultations to recommend the most suitable procedure based on your individual needs and goals.",
       },
       {
-        question: "How long does recovery take?",
-        answer: "Recovery takes about 1-2 weeks.",
+        question: "What is the recovery time after breast surgery?",
+        answer:
+          "Recovery varies, but most patients can resume light activities within 1-2 weeks, with full recovery taking 4-6 weeks depending on the procedure. We provide clear aftercare instructions for a smooth recovery.",
+      },
+      {
+        question: "Will there be visible scars after surgery?",
+        answer:
+          "Some scarring is expected, but our advanced techniques minimize and place them in less visible areas. Over time, scars fade, and we offer treatments to support optimal healing.",
+      },
+      {
+        question: "Can breast surgery affect breastfeeding?",
+        answer:
+          "Breast implants usually don't interfere with breastfeeding, while lifts or reductions may have an impact based on the amount of tissue adjusted, so it's best to discuss with your surgeon.",
+      },
+      {
+        question: "How long do the results last?",
+        answer:
+          "Breast surgery results can be long-lasting, though factors like aging or weight changes can affect them. Following your surgeon's advice and maintaining a healthy lifestyle helps prolong the results.",
+      },
+      {
+        question: "How soon can I return to normal activities after surgery?",
+        answer:
+          "You can usually resume light activities within 1-2 weeks, but avoid strenuous exercise or heavy lifting for about 4-6 weeks. Your surgeon will guide you through your personalized recovery plan.",
       },
     ],
     stateKey: "gallery1",
@@ -38,13 +56,39 @@ const buttonsData = [
     image2: "/BeforeAfter/bbl2.webp",
     faqs: [
       {
-        question: "What is Gynecomastia?",
+        question:
+          "What is a Brazilian Butt Lift, and how does it differ from butt implants?",
         answer:
-          "Gynecomastia is a condition involving enlarged breast tissue in men.",
+          "A BBL uses your own fat for natural-looking butt enhancement, while implants are synthetic.",
       },
       {
-        question: "How long does recovery take?",
-        answer: "Recovery takes about 1-2 weeks.",
+        question:
+          "How long will it take to recover after a BBL, and when can I sit or sleep comfortably again?",
+        answer:
+          "Recovery takes 2-4 weeks, and special precautions for sitting or sleeping are necessary during this time.",
+      },
+      {
+        question:
+          "Are there any risks associated with a BBL, and how can I ensure a safe and successful outcome?",
+        answer:
+          "Risks are minimized when performed by a skilled surgeon. Following aftercare instructions is crucial for safety.",
+      },
+      {
+        question:
+          "Can I combine a BBL with other surgeries like liposuction or a tummy tuck to improve my overall body shape?",
+        answer:
+          "Yes, many patients combine BBL with other procedures for more comprehensive body contouring.",
+      },
+      {
+        question:
+          "What can I do to maintain my BBL results, and will I need follow-up treatments in the future?",
+        answer:
+          "Maintain a stable weight and follow a healthy lifestyle to keep your BBL results long-lasting. Follow-up treatments are not usually necessary.",
+      },
+      {
+        question: "How long will it take to see the final results of my BBL?",
+        answer:
+          "While you will notice an immediate change in shape, the final results typically become visible after 3-6 months, once the swelling subsides and your body adjusts to the fat transfer",
       },
     ],
     stateKey: "gallery2",
@@ -55,13 +99,38 @@ const buttonsData = [
     image2: "/BeforeAfter/totalbody2.webp",
     faqs: [
       {
-        question: "What is 360 Body Lifting?",
+        question: "What is the recovery time for total body reshaping?",
         answer:
-          "360 Body Lifting helps to remove excess skin and fat after major weight loss.",
+          "Recovery can take several weeks, with most patients returning to light activities within 2-3 weeks, while full recovery and the final results may take a few months.",
       },
       {
-        question: "How long does recovery take?",
-        answer: "Recovery takes about 2-3 weeks.",
+        question:
+          "Will I experience significant weight loss from total body reshaping?",
+        answer:
+          "Total body reshaping is more about contouring and improving your body’s shape rather than weight loss. It’s best suited for patients who have already reached their target weight.",
+      },
+      {
+        question: "How long will the results of total body reshaping last?",
+        answer:
+          "The results can be long-lasting as long as you maintain a stable weight and healthy lifestyle. However, natural aging may still affect your body’s appearance over time.",
+      },
+      {
+        question:
+          "What exactly is total body reshaping, and how can it help me achieve my desired body shape?",
+        answer:
+          "Total body reshaping combines multiple procedures to contour and improve your body's proportions, targeting stubborn fat, loose skin, or muscle tone.",
+      },
+      {
+        question:
+          "Will the results of total body reshaping be permanent, or will I need touch-ups in the future?",
+        answer:
+          "Results are long-lasting if you maintain a stable weight, but natural aging may still affect the body over time.",
+      },
+      {
+        question:
+          "How can I maintain my results after total body reshaping, and what lifestyle changes will I need to make?",
+        answer:
+          "To maintain your results, it's essential to follow a balanced diet, engage in regular exercise, and avoid significant weight fluctuations, which can affect the long-term outcome of your body reshaping procedures.",
       },
     ],
     stateKey: "gallery3",
