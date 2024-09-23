0fc4f96fd51c9cbf6068cccf11371e16987f4dbd
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-09-23 16:39:41 +0300

    Update VideoLibrary.tsx

--- src/components/spaghetti/VideoLibrary.tsx
+++ src/components/spaghetti/VideoLibrary.tsx
@@ -15,45 +15,34 @@ const items: MenuItem[] = [
   {
     key: "1",
     icon: <MailOutlined />,
-    label: "Navigation One",
+    label: "BBL",
     children: [
       { key: "11", label: "Option 1" },
       { key: "12", label: "Option 2" },
-      { key: "13", label: "Option 3" },
-      { key: "14", label: "Option 4" },
     ],
   },
   {
     key: "2",
     icon: <AppstoreOutlined />,
-    label: "Navigation Two",
+    label: "Breast",
     children: [
-      { key: "21", label: "Option 1" },
-      { key: "22", label: "Option 2" },
+      { key: "21", label: "Breast Implant" },
+      { key: "22", label: "Breast Reconstruction" },
       {
         key: "23",
-        label: "Submenu",
+        label: "Breast Lift",
         children: [
-          { key: "231", label: "Option 1" },
-          { key: "232", label: "Option 2" },
-          { key: "233", label: "Option 3" },
+          { key: "231", label: "Breast Lift" },
+          { key: "232", label: "Breast Lift with Implants" },
         ],
       },
-      {
-        key: "24",
-        label: "Submenu 2",
-        children: [
-          { key: "241", label: "Option 1" },
-          { key: "242", label: "Option 2" },
-          { key: "243", label: "Option 3" },
-        ],
-      },
+      { key: "24", label: "Breast Reduction" },
     ],
   },
   {
     key: "3",
     icon: <SettingOutlined />,
-    label: "Navigation Three",
+    label: "Total Body",
     children: [
       { key: "31", label: "Option 1" },
       { key: "32", label: "Option 2" },
@@ -113,13 +102,22 @@ export default function VideoLibrary() {
   };
 
   return (
-    <Menu
-      mode="inline"
-      defaultSelectedKeys={["231"]}
-      openKeys={stateOpenKeys}
-      onOpenChange={onOpenChange}
-      style={{ width: 256 }}
-      items={items}
-    />
+    <div className="flex flex-row">
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["231"]}
+        openKeys={stateOpenKeys}
+        onOpenChange={onOpenChange}
+        style={{ width: "100%", maxWidth: "256px" }}
+        items={items}
+      />
+      <div className="flex flex-col justify-end">
+        <video
+          className="z-50"
+          src="/video/animations/breast/Breast Lift.mp4"
+          controls
+          style={{ width: "100%", maxWidth: "500px", height: "auto" }}></video>
+      </div>
+    </div>
   );
 }
