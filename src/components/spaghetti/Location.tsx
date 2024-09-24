a59107ef1f5e659be0cce583a39d23a4b884f348
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-09-24 16:58:54 +0300

    location a bit more responsive

--- src/components/spaghetti/Location.tsx
+++ src/components/spaghetti/Location.tsx
@@ -8,8 +8,8 @@ import {
 
 export default function Location() {
   return (
-    <div>
-      <div className="w-full flex">
+    <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="flex w-full max-w-[400px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10133147.836277347!2d29.356114206454567!3d42.49165901949489!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac64c07925733%3A0x96ea14ba8be2cdb6!2sEstetik%20International!5e0!3m2!1sen!2str!4v1726670444497!5m2!1sen!2str"
           loading="lazy"
@@ -53,7 +53,7 @@ export default function Location() {
         </div>
       </div>
       <video
-        className="h-auto p-5 w-full rounded-[2rem]"
+        className="h-auto p-5 w-full rounded-[2rem] max-w-[400px]"
         src="/video/slider.mp4"
         controls
         poster="/img/dental2.png"></video>
