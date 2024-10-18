6f0e8631fbb1a199f7573c63e3e031e1248404c9
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-10-18 17:53:50 +0300

    Update Navbar.tsx

--- src/components/spaghetti2/Navbar.tsx
+++ src/components/spaghetti2/Navbar.tsx
@@ -1,45 +1,51 @@
-import { MessageOutlined, PhoneOutlined, WhatsAppOutlined } from "@ant-design/icons";
+import {
+  MessageOutlined,
+  PhoneOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
 import { Button } from "antd";
 import Image from "next/image";
 
-export default function Navbar(){
-
-    return(
+export default function Navbar() {
+  return (
     <div className="w-full h-[10%] bg-white flex flex-row sticky top-0 z-50">
-      <div className="flex justify-start">
-        <Image 
-          src="/img/estintlogo25.webp" 
+      <div className="hidden md:flex justify-start">
+        <Image
+          src="/img/estintlogo25.webp"
           alt="Logo"
-          width={160} 
-          height={80} 
-          className="h-auto ml-32"
+          width={160}
+          height={80}
+          className="h-auto ml-4 sm:ml-8 md:ml-32"
         />
       </div>
+
       <div className="flex-grow"></div>
-      <div className="flex flex-row justify-end gap-8 mr-32">
+
+      <div className="flex flex-row justify-end items-center gap-4 sm:gap-6 md:gap-8 mr-4 sm:mr-8 md:mr-32">
         <Button
-          className="h-[70%] my-auto bg-green-500 px-2 py-0"
+          className="h-[90%] sm:h-[70%] bg-[#25D366] px-2 py-1 sm:py-0 rounded-[25px]"
           target="_blank"
-          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F">
+          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
+        >
           WhatsApp
-          <WhatsAppOutlined className="text-white text-4xl p-1" />
+          <WhatsAppOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
         </Button>
         <Button
-          className="h-[70%] my-auto bg-[#424864] px-2 py-0"
+          className="h-[90%] sm:h-[70%] bg-[#424864] px-2 py-1 sm:py-0 rounded-[25px]"
           target="_blank"
-          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F">
+          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
+        >
           iMessage
-          <MessageOutlined className="text-white text-4xl p-1" />
+          <MessageOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
         </Button>
         <Button
-          className="h-[70%] my-auto bg-orange-600 px-2 py-0"
-          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F">
+          className="h-[90%] sm:h-[70%] bg-orange-600 px-2 py-1 sm:py-0 rounded-[25px]"
+          href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
+        >
           Call Us
-          <PhoneOutlined className="text-white text-4xl p-1" />
+          <PhoneOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
         </Button>
-        </div>
+      </div>
     </div>
-
-    )
-
+  );
 }
