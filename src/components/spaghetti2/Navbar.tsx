c25afdb8ae47c8f563dc7cce84bf7cc3898dc720
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-10-19 14:21:16 +0300

    center nav's button icons

--- src/components/spaghetti2/Navbar.tsx
+++ src/components/spaghetti2/Navbar.tsx
@@ -23,27 +23,33 @@ export default function Navbar() {
 
       <div className="flex flex-row justify-end items-center gap-6 lg:gap-8 mr-4 lg:mr-32">
         <Button
-          className="h-[90%] sm:h-[70%] bg-[#25D366] px-2 py-1 sm:py-0 rounded-[25px]"
+          className="h-[90%] sm:h-[70%] bg-[#25D366] px-2 py-1 sm:py-0 rounded-[25px] flex items-center justify-center"
           target="_blank"
           href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
         >
-          WhatsApp
-          <WhatsAppOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
+          <span className="flex items-center gap-2">
+            WhatsApp
+            <WhatsAppOutlined className="text-white text-2xl sm:text-3xl md:text-4xl" />
+          </span>
         </Button>
         <Button
-          className="h-[90%] sm:h-[70%] bg-[#424864] px-2 py-1 sm:py-0 rounded-[25px]"
+          className="h-[90%] sm:h-[70%] bg-[#424864] px-2 py-1 sm:py-0 rounded-[25px] flex items-center justify-center"
           target="_blank"
           href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
         >
-          iMessage
-          <MessageOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
+          <span className="flex items-center gap-2">
+            iMessage
+            <MessageOutlined className="text-white text-2xl sm:text-3xl md:text-4xl" />
+          </span>
         </Button>
         <Button
-          className="h-[90%] sm:h-[70%] bg-orange-600 px-2 py-1 sm:py-0 rounded-[25px]"
+          className="h-[90%] sm:h-[70%] bg-orange-600 px-2 py-1 sm:py-0 rounded-[25px] flex items-center justify-center"
           href="https://wa.me/905498287202?text=YDG3400EE%20Hello%20Estetik%20International%20Health%20Group%20Team.%20Can%20I%20book%20a%20consultation%3F"
         >
-          Call Us
-          <PhoneOutlined className="text-white text-2xl sm:text-3xl md:text-4xl p-1" />
+          <span className="flex items-center gap-2">
+            Call Us
+            <PhoneOutlined className="text-white text-2xl sm:text-3xl md:text-4xl" />
+          </span>
         </Button>
       </div>
     </div>
