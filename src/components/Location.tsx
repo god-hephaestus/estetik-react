ea10e6cb926732813607b13cde7a12acecb0b430
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-10-24 11:14:07 +0300

    create Location.tsx

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,11 +1,61 @@
+import {
+  FacebookFilled,
+  InstagramFilled,
+  LinkedinFilled,
+  TikTokFilled,
+  YoutubeFilled,
+} from "@ant-design/icons";
+
 export default function Location() {
   return (
-    <div className="object-contain w-[90%] h-[20%]">
+    <div className="object-contain h-full">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10133147.836277347!2d29.356114206454567!3d42.49165901949489!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac64c07925733%3A0x96ea14ba8be2cdb6!2sEstetik%20International!5e0!3m2!1sen!2str!4v1726670444497!5m2!1sen!2str"
         loading="lazy"
-        className="h-full"
+        className="h-[80%] w-full"
       ></iframe>
+      <div className="flex flex-row justify-between w-full items-end bg-[#D0EEEC] rounded-[10px] mt-4">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/estetikinternational/"
+          title="Instagram"
+          rel="noopener noreferrer"
+        >
+          <InstagramFilled style={{ fontSize: "40px", color: "#7ac8c3" }} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/estetikinternational"
+          title="Facebook"
+          rel="noopener noreferrer"
+        >
+          <FacebookFilled style={{ fontSize: "40px", color: "#7ac8c3" }} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.youtube.com/@EstetikInternationalcomtr"
+          title="YouTube"
+          rel="noopener noreferrer"
+        >
+          <YoutubeFilled style={{ fontSize: "40px", color: "#7ac8c3" }} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/company/5168206/"
+          title="LinkedIn"
+          rel="noopener noreferrer"
+        >
+          <LinkedinFilled style={{ fontSize: "40px", color: "#7ac8c3" }} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.tiktok.com/@estetik.international"
+          title="TikTok"
+          rel="noopener noreferrer"
+        >
+          <TikTokFilled style={{ fontSize: "40px", color: "#7ac8c3" }} />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/OperationForm.tsx
+++ src/components/OperationForm.tsx
@@ -90,19 +90,14 @@ export default function OperationForm() {
   };
 
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex justify-center items-center h-full ">
       <Form
         name="operationForm"
         form={form}
         layout="vertical"
         onFinish={handleSubmit}
-        className="w-full h-full flex-1 max-w-lg px-6 bg-[#d0eeec] rounded-[25px] border-2 border-[#d0eeec] shadow-md"
+        className="w-full h-full flex-1 px-6 bg-[#d0eeec] rounded-[25px] border-2 border-[#d0eeec] shadow-md"
       >
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10133147.836277347!2d29.356114206454567!3d42.49165901949489!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac64c07925733%3A0x96ea14ba8be2cdb6!2sEstetik%20International!5e0!3m2!1sen!2str!4v1726670444497!5m2!1sen!2str"
-          loading="lazy"
-          className="w-full my-4 h-56"
-        ></iframe>
         <Form.Item
           label="Name"
           name="name"
