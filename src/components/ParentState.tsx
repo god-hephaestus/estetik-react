f7c47dfa25186a3bbc439affd2d6ceb2cea3590e
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2024-10-31 09:46:44 +0300

    Update ParentState.tsx

--- src/components/ParentState.tsx
+++ src/components/ParentState.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Comparison from "./Comparison";
 import Gallery from "./Gallery";
 import Testimonials from "./Testimonials";
