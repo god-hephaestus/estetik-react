aabeb8665b1553988bece7be82e46eaff0d9c0b4
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-11-01 10:51:29 +0300

    form modal

--- src/components/VideoSliderMobile.tsx
+++ src/components/VideoSliderMobile.tsx
@@ -4,6 +4,7 @@ import { FormOutlined, LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Button, Carousel } from "antd";
 import { useState } from "react";
 import { createPortal } from "react-dom";
+import OperationForm from "./OperationForm";
 
 export default function VideoSliderMobile({
   videoDescription,
@@ -21,6 +22,7 @@ export default function VideoSliderMobile({
 
   const [isOpen, setIsOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showFormModal, setShowFormModal] = useState(false);
   const [activeVideoSrc, setActiveVideoSrc] = useState<string | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
@@ -40,6 +42,15 @@ export default function VideoSliderMobile({
     setIsOpen(false);
     setTimeout(() => setShowModal(false), 300);
     setTimeout(() => setActiveVideoSrc(null), 300);
+  };
+
+  const openFormModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setShowFormModal(true);
+  };
+
+  const closeFormModal = () => {
+    setShowFormModal(false);
   };
 
   const handleNextVideo = () => {
@@ -68,7 +79,7 @@ export default function VideoSliderMobile({
         >
           <Button
             className="absolute bottom-2 h-[40px] rounded-[25px] z-[99] border-[#13a89e]"
-            onClick={(e) => e.stopPropagation()}
+            onClick={openFormModal}
           >
             Consultation
             <span className="rounded-full flex -mr-3 justify-center items-center w-8 h-8 bg-white">
@@ -143,6 +154,28 @@ export default function VideoSliderMobile({
                   autoPlay
                 ></video>
               </div>
+            </div>
+          </div>,
+          document.body
+        )}
+
+      {showFormModal &&
+        createPortal(
+          <div
+            className="fixed inset-0 z-[9999] flex items-center justify-center bg-black bg-opacity-70 transition-opacity duration-[300ms] ease-in-out"
+            onClick={closeFormModal}
+          >
+            <div
+              className="relative  rounded-[25px]  shadow-lg w-11/12 max-w-lg transform transition-transform duration-[300ms] scale-100"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <button
+                onClick={closeFormModal}
+                className="absolute top-2 right-2 text-black text-2xl font-bold focus:outline-none"
+              >
+                &times;
+              </button>
+              <OperationForm isExpanded />
             </div>
           </div>,
           document.body
